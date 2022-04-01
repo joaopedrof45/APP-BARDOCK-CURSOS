9d44f32f66040bbb37273570db9b8d3591dee22a
João Pedro <joao-pedro--f@hotmail.com>
2022-04-01 16:31:24 -0300

    trazendo dados de assistidos , no endponit de todos users

--- App.js
+++ App.js
@@ -1,22 +1,56 @@
 import React, { Component } from 'react'
-import { FlatList } from 'react-native'
-import Styles from './styles'
-import cursos from './src/cursos'
-import { render } from 'react-dom'
+import {SafeAreaView,Text,StyleSheet,FlatList } from 'react-native'
+import api from './src/Services/api'
+import Curso from './src/Pages/Curso'
+import { Form } from 'reactstrap'
+
 
 class CursosList extends Component {
-  state = {
-    cursos: this.cursos
+ 
+  constructor(props){
+    super(props);
+    this.state={
+      cursos:[]
+    }
   }
 
+  async componentDidMount(){
+    const response = await api.get('pessoasassistidas/?page=2&page_size=1'
+    ,
+{     auth: {
+      username: 'est.joao.f',
+      password: 'Lamburguini456'
+    },
+}
+    );
+    this.setState({
+      cursos:response.data.results
+    });
+    console.log(this.state.cursos)
+  }
 
   render() {
     return (
-      <ul>
-        { this.state.cursos.map(cursos => <li>{cursos}</li>)}
-      </ul>
+      
+      <SafeAreaView style={styles.container}>
+        <Form action="#"> 
+          <FlatList data={this.state.cursos} renderItem={({item})=><Curso data={item}/>}
+        /> 
+        </Form>
+       
+      </SafeAreaView>
     )
   }
 }
+
+
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor:'black',
+    flex:1
+
+  }
+})
+
  
 export default CursosList
